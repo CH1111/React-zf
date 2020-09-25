1d76b5b7a8db65f647a3437e967b5db44d0b9c7b
unknown <1120078353@qq.com>
2020-09-25 17:16:31 +0800

    basic React-Router settings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from "react";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return <div>租房项目</div>;
-  }
-}
-
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CityLists from "./pages/CityLists";
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <ul>
+          <li>
+            <Link to="/home">租房首页</Link>
+          </li>
+          <li>
+            <Link to="/citylists">城市选择</Link>
+          </li>
+        </ul>
+        <Route path="/home" exact component={Home} />
+        <Route path="/citylists" component={CityLists} />
+      </div>
+    </Router>
+  );
+};
 export default App;
---
+++ src/pages/CityLists/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class CityLists extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>CityLists页面</div>;
+  }
+}
+
+export default CityLists;
